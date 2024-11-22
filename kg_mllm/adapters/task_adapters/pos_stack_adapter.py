cecab79d6eff2035b0393ac41265c638fa09a84c
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2024-11-22 14:14:11 -0500

    Add training file

--- kg_mllm/adapters/task_adapters/pos_stack_adapter.py
+++ kg_mllm/adapters/task_adapters/pos_stack_adapter.py
@@ -1,10 +1,9 @@
-import evaluate
-import numpy as np
-from adapters import AdapterConfig, AdapterTrainer, AutoAdapterModel
+from adapters import AdapterConfig, AutoAdapterModel
 from adapters.composition import Stack
-from transformers import AutoConfig, AutoTokenizer, TrainingArguments
+from transformers import AutoConfig, AutoTokenizer
 
 from kg_mllm.test import evaluate_model
+from kg_mllm.train import train_model
 from kg_mllm.util.data import load_train_val_test
 
 # TODO: Consolidate and move to config
@@ -13,13 +12,6 @@ output_dir = './training_output'
 adapter_dir = ''
 adapter_config = 'BAR'
 model_name = 'bert-base-multilingual-cased'
-learning_rate = 1e-4
-num_train_epochs = 50
-per_device_train_batch_size = 32
-per_device_eval_batch_size = 32
-evaluation_strategy = 'epoch'
-save_strategy = 'no'
-weight_decay = 0.01
 
 
 def create_model():
@@ -50,45 +42,9 @@ def create_model():
     return model
 
 
-def main() -> None:
-    model = create_model()
-
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-multilingual-cased')
-    train_dataset, val_dataset, test_dataset = load_train_val_test(tokenizer)
-
-    training_args = TrainingArguments(
-        learning_rate=learning_rate,
-        num_train_epochs=num_train_epochs,
-        per_device_train_batch_size=per_device_train_batch_size,
-        per_device_eval_batch_size=per_device_eval_batch_size,
-        evaluation_strategy=evaluation_strategy,
-        save_strategy=save_strategy,
-        weight_decay=weight_decay,
-        output_dir=output_dir,
-        overwrite_output_dir=True,
-        save_total_limit=1,
-        load_best_model_at_end=True,
-    )
-
-    f1_metric = evaluate.load('f1')
-
-    trainer = AdapterTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=val_dataset,
-        compute_metrics=lambda pred: {
-            'f1': f1_metric.compute(
-                predictions=np.argmax(pred.predictions, axis=-1),
-                references=pred.label_ids,
-                average='macro',
-            )['f1'],
-        },
-    )
-
-    trainer.train()
-    evaluate_model(trainer, test_dataset, output_dir)
-
-
 if __name__ == '__main__':
-    main()
+    model = create_model()
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    train_dataset, val_dataset, test_dataset = load_train_val_test(tokenizer)
+    trainer = train_model(model, train_dataset, val_dataset)
+    evaluate_model(trainer, test_dataset, output_dir)
